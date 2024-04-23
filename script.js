65b466223931e7f3b48b7214628bdd6ace230b8b
aangelco <152907977+aangelco@users.noreply.github.com>
2024-04-23 17:43:56 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function divClick(var1) {
+  
+  console.log(var1)
+  
+  document.getElementsByClassName('show')[0].className='noshow';
+  
+  document.getElementById(var1).className="show";
+}
 
+function moveMole() {
+  var div = document.querySelector('.mole');
+  var maxX = window.innerWidth - div.offsetWidth;
+  var maxY = window.innerHeight - div.offsetHeight;
+
+  var newX = Math.floor(Math.random() * maxX);
+  var newY = Math.floor(Math.random() * maxY);
+
+  div.style.left = newX + 'px';
+  div.style.top = newY + 'px';
+}
+
+var intervalId = setInterval(moveMole, 500);
+
+function start() {
+  var donutDiv = document.getElementById("donutDiv");
+
+  if (donutDiv) {
+    donutDiv.classList.add("animation");
+    document.getElementById('donutDiv').id = 'stopDiv';
+  } else {
+      stopDiv.classList.add("box2picAfter");
+      stopDiv.classList.add("animationAfter");
+      } 
+}
